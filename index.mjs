e2a4c2d179a79c2a499dd256ba8747a525e0985a
code-alt <altthatguy@gmail.com>
2022-08-06 07:23:26 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,17 +1,58 @@
 import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
-import nodeStatic from 'node-static';
-
-
+import { existsSync, readFile } from 'node:fs';
+import url, {fileURLToPath} from 'node:url';
 const bare =  createServer('/bare/');
-const serve = new nodeStatic.Server('static/');
 const server = http.createServer();
+import path from 'node:path';
+
+function isPathFileAvaliable(str) {
+	return str.substring(str.lastIndexOf('.')).includes('.');
+}
+
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
 
 server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
+  if (bare.shouldRoute(req)) {
 		bare.routeRequest(req, res);
 	} else {
-		serve.serve(req, res);
+		var requestp = url.parse(req.url).pathname;
+		var reqstat = _dirname + "/static" + requestp;
+		var ip = '"not avaliable"';
+		if (existsSync(reqstat) && requestp.split('').pop() != "/" && (isPathFileAvaliable(reqstat) == true)) {
+			res.writeHead(200);
+			readFile(reqstat, function read(err, data) {
+    		if (err) {
+        	console.error(err);
+    		}
+    		res.end(data);
+				console.log("Request on static resources for " + requestp + " at IP " + ip);
+			});
+			return;
+		} else if (isPathFileAvaliable(reqstat) == false && existsSync(reqstat + "/index.html") && existsSync(reqstat)) {
+			res.writeHead(200);
+			readFile(reqstat + "/index.html", function read(err, data) {
+    		if (err) {
+        	console.error(err);
+    		}
+    		res.end(data);
+				console.log("Request on static resources for " + requestp + " at IP " + ip);
+			});
+			return;
+		} else if (existsSync(reqstat)) {
+			res.writeHead(200);
+			readFile(reqstat, function read(err, data) {
+    		if (err) {
+        	console.error(err);
+    		}
+    		res.end(data);
+				console.log("Request on static resources for " + requestp + " at IP " + ip);
+			});
+		} else {
+			res.writeHead(200);
+    	res.end("We are sorry, but we cannot process your request at this time.");
+			return false;
+		}
 	}
 });
 
